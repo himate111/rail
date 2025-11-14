d0d887ff95ea07da2d79af45e9ab51ca0d3f171c
himate111 <sivashankar.mk16@gmail.com>
2025-11-14 17:58:50 +0530

    Delete frontend-react directory

--- frontend-react/src/api/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://attendance-tracking-system-nu.vercel.app/", // uses the proxy to hit your backend
-});
-
-export default api;
